716ddc580798c2a0f2ce635964734aa14141ecf3
RaghadAbuSamor <raghadmohammad253@gmail.com>
2022-11-29 09:53:56 +0200

    Final update

--- dwitter/views.py
+++ dwitter/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Profile
+from django.shortcuts import render, redirect
 # Create your views here.
 from .forms import DweetForm
 from django.shortcuts import render
@@ -10,6 +11,7 @@ def dashboard(request):
            dweet = form.save(commit=False)
            dweet.user = request.user
            dweet.save()
+           return redirect("dwitter:dashboard")
     form = DweetForm()
     return render(request, "dwitter/dashboard.html", {"form": form})
 
@@ -17,19 +19,20 @@ def profile_list(request):
     profiles = Profile.objects.exclude(user=request.user)
     return render(request, "dwitter/profile_list.html", {"profiles": profiles})
 
-def profile(request, pk):
-    if not hasattr(request.user, 'profile'):
-        missing_profile = Profile(user=request.user)
-        missing_profile.save()
-    profile = Profile.objects.get(pk=pk)
+def dashboard(request):
+    form = DweetForm(request.POST or None)
     if request.method == "POST":
-        current_user_profile = request.user.profile
-        data = request.POST
-        action = data.get("follow")
-        if action == "follow":
-            current_user_profile.follows.add(profile)
-        elif action == "unfollow":
-            current_user_profile.follows.remove(profile)
-        current_user_profile.save()
-    return render(request, "dwitter/profile.html", {"profile": profile})
+        if form.is_valid():
+           dweet = form.save(commit=False)
+           dweet.user = request.user
+           dweet.save()
+           return redirect("dwitter:dashboard")
+    followed_dweets = Dweet.objects.filter(
+        user__profile__in=request.user.profile.follows.all()
+    ).order_by("-created_at")
+    return render(
+        request,
+        "dwitter/dashboard.html",
+        {"form": form, "dweets": followed_dweets},
+    )
 
